39c9f6de26e06ede278e53ef15d895e2f33dc41f
EmileBedard <emile.bed@outlook.fr>
2026-01-22 17:57:33 -0500

    in progress of task 2 question 3.2. I need help and i have a few questions

--- task2/task-2-js/script.js
+++ task2/task-2-js/script.js
@@ -213,13 +213,13 @@ NodeList
     /*************************************** */
     /* 2: GRID OF BOXES */
     /* 2A: Create another new function: function customNewBoxCreate(parent){ //body }*/
-    /* 2B: In the body of customNewBoxCreate create a new div element, that has the class testDiv. 
+    /* 2B: In the body of customNewBoxCreate create a new div element, that has the class testDiv.
     /* 2C:Then append this new element to the parent variable within the function. 
     /* 2D:Finally, return</code> this new element */
-    /* 2E:Create a nested for loop (for rows and columns) to iterate through 10 columns and 10 rows (just like the JS Review :)). 
+    /* 2E:Create a nested for loop (for rows and columns) to iterate through 10 columns and 10 rows (just like the JS Review :)).
         Call the customNewBoxCreate function, in order to generate a new div -> representing each cell in the grid. 
         Ensure that the parent element for each of these new divs is the element whose id is named `new-grid`*/
-    /* 2F: You will see at this point that the x,y position of the resulting divs makes no sense... 
+    /* 2F: You will see at this point that the x,y position of the resulting divs makes no sense...
         Fix this by doing the following: every time you call customNewBoxCreate() - save the current returned element 
         in a variable i.e. returnedDiv. 
         Set the style (left and top) to the of this element to 
@@ -231,6 +231,21 @@ NodeList
         otherwise lat it have the content `ODD`.*/
 
     /***CODE */
+    for (let i = 0; i < 10; i++) {
+
+        customNewBoxCreate(document.querySelector("#new-grid"));
+        // how to place the different cubes in the html grid layout ??
+        // why return in the custom function ? how does it work ?
+
+
+    }
+
+    function customNewBoxCreate(parent) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("testDiv");
+        parent.appendChild(newDiv);
+        return;
+    }
 
 
     /***EXPLANATION::
