62006ef0494d0496f63ecae107f4d129560d32b3
EmileBedard <emile.bed@outlook.fr>
2026-01-27 16:47:51 -0500

    used mouse move and keydown

---
+++ learning material/js+html2/html/js/keyEx.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    console.log("keys");
+
+    let speedX = 5;
+    let boxA = document.querySelector("#boxA");
+
+    // anonymous function here
+    window.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowRight") {
+            console.log(parseInt(boxA.style.left))
+            let currentPosition = parseInt(boxA.style.left);
+
+            boxA.style.left = currentPosition + speedX + "px";
+        }
+
+        if (e.key === "ArrowLeft") {
+            console.log(parseInt(boxA.style.left))
+            let currentPosition = parseInt(boxA.style.left);
+
+            boxA.style.left = currentPosition - speedX + "px";
+        }
+
+    })
+}
