d0deff7587055e08895a1935ccf1632a92396c49
EmileBedard <emile.bed@outlook.fr>
2026-02-03 14:37:32 -0500

    finished the js+html III learning materials

--- learning material/js+html3/WEEK-4-START/js/week4.js
+++ learning material/js+html3/WEEK-4-START/js/week4.js
@@ -1,4 +1,16 @@
 window.onload = setup;
 function setup() {
   console.log("in week 4 ;)")
+
+  document.querySelector("#boxA").addEventListener("click", runTimeOut);
+
+  function runTimeOut() {
+    window.setTimeout(addTimeoutText, 2000);
+
+  }
+
+  function addTimeoutText() {
+    let parent = document.getElementById("parent");
+    parent.innerHTML += " NEW TEXT TO APPEAR ";
+  }
 }
