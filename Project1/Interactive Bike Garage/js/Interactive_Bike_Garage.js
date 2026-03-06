8398e60310b0c0d65b9eb5f763d42a25a1c7e954
EmileBedard <emile.bed@outlook.fr>
2026-03-05 19:23:31 -0500

    added the context menu window to enter bike details and i'm currently working on storing and retrieving data from local storage with arrays, JSON and keys

--- Project1/Interactive Bike Garage/js/Interactive_Bike_Garage.js
+++ Project1/Interactive Bike Garage/js/Interactive_Bike_Garage.js
@@ -7,24 +7,85 @@ let garage = {
 
 
 window.onload = function () {
-
+    loadAllBikes();
 
     let addBikeButton = document.querySelector(".addBikeButton");
-    let displayBikeButton = document.querySelector(".applyBikeButton");
-
-    displayBikeButton.addEventListener("click", function () {
-
-
-    })
+    let popWindow = document.querySelector(".bikeDetailsWindow")
 
     addBikeButton.addEventListener("click", function () {
+        popWindow.style.display = "inline-block";
+
+    });
+
+    let bikeForm = document.getElementById("bikeForm");
+
+    let key = 0
+
+    bikeForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        let name = document.getElementById("nameInput").value;
+        let type = document.getElementById("typeInput").value;
+        let color = document.getElementById("colorInput").value;
+
+        console.log(name, type, color);
+
+        let bikeData = {
+            bikename: name,
+            biketype: type,
+            bikecolor: color
+        };
+
+        popWindow.style.display = "none";
+
+        localStorage.setItem(key, JSON.stringify(bikeData));
+
+        key += 1;
 
         garage.numBikes += 1;
         console.log("added1bike");
 
-        let newBike = new Bike("Opus Vivace", "Road", "red");
+
+        for (let i = 0; i < storedBikes.length; i++) {
+
+            console.log(storedBike);
+        }
+
+        let indexedBikeArray = JSON.parse(value)
+
+        for (let storedValue of indexedBikeArray) {
+            console.log(storedValue);
+        }
+
+        let newBike = new Bike();
 
         garage.bikes.push(newBike);
         newBike.renderbike();
-    });
+
+    })
+
+    function loadAllBikes() {
+        let keys = Object.keys(localStorage);
+
+        for (let i = 0; i < keys.length; i++) {
+            let key = keys[i];
+
+            let bikeDataString = localStorage.getItem(key);
+            let bikeData = JSON.parse(bikeDataString);
+
+            // 4. On crée l'instance et on l'affiche
+            let newBike = new Bike(
+                bikeData.bikename,
+                bikeData.biketype,
+                bikeData.bikecolor
+            );
+
+            garage.bikes.push(newBike);
+            newBike.renderbike();
+
+
+        }
+    }
+
+
 }
