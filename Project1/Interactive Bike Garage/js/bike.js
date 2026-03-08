1b29bb62638008f3cd3f6085787b41bc22ac4db2
EmileBedard <emile.bed@outlook.fr>
2026-03-07 20:31:47 -0500

    added color and name placement when we click on a made bike!

--- Project1/Interactive Bike Garage/js/bike.js
+++ Project1/Interactive Bike Garage/js/bike.js
@@ -11,7 +11,7 @@ class Bike {
 
         this.bike = document.createElement("div");
         this.bike.classList.add("bikeObject")
-        this.bike.id = (`bike${id}`);
+        this.bike.id = (id);
 
         this.bikeButton = document.createElement("button");
         this.bikeButton.innerHTML = `<p>${this.name}</p> <img src="assets/small bike element white.svg" alt="bike">`;
--- Project1/Interactive Bike Garage/js/registerBikeParts.js
+++ Project1/Interactive Bike Garage/js/registerBikeParts.js
@@ -1,12 +1,55 @@
-let bikeContainers = document.querySelectorAll("bikeObjects");
-let toolbox = document.querySelector("toolbox");
+
+let toolbox = document.querySelector(".toolbox");
 
 
-for (let i = 0; i < bikeContainers.length; i++) {
-    let bikeContainer = bikeContainers[i];
-    bikeContainer.addEventListener("click", registerParts());
-}
+document.addEventListener("click", function (event) {
 
-function registerParts() {
+    if (event.target.closest(".bikeObject")) {
 
-}
+        console.log(event.target.closest(".bikeObject").id);
+
+        let bikeId = event.target.closest(".bikeObject").id
+
+        let bikeDataString = localStorage.getItem(bikeId);
+        let fetchedData = JSON.parse(bikeDataString);
+        console.log(fetchedData);
+
+        let infographicTitle = document.getElementById("infographic-name")
+        infographicTitle.style.color = fetchedData.bikeColor;
+        infographicTitle.innerHTML = "Anatomy Of " + fetchedData.bikeName;
+
+        let emptyFrameSVG = document.getElementById("empty-frame").style.stroke = fetchedData.bikeColor
+
+        let toolboxBikeName = document.querySelector(".bike-name-placeholder");
+        toolboxBikeName.innerHTML = fetchedData.bikeName;
+        toolboxBikeName.style.color = fetchedData.bikeColor;
+
+
+        //displays and move the view to the parts registry zone
+        let partsZone = document.querySelector(".partsZone");
+        partsZone.style.display = "grid";
+        partsZone.scrollIntoView({ behavior: "smooth" });
+
+    }
+
+}); // code here written with the help of chatGPT. I use event delegation to make sure I attach the event listeners only after the bikeobjects are created. My issue in my original code commented below was that my bikeContainers array was empty because the code ran before the bikes were created.
+
+// function setupBikeListeners() {
+//     for (let i = 0; i < bikeContainers.length; i++) {
+
+//         let bikeContainer = bikeContainers[i];
+
+//         bikeContainer.addEventListener("click", function () {
+
+//             let partsZone = document.querySelector(".partsZone");
+
+//             console.log("bike object clicked!");
+
+//             partsZone.style.display = "grid";
+
+
+
+//         });
+//     };
+// };
+
