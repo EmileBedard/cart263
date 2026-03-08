993b508fa4228eba188121ed7fab1deafce30a92
EmileBedard <emile.bed@outlook.fr>
2026-03-07 22:00:20 -0500

    finished the parts registry systems and page !!!!

--- Project1/Interactive Bike Garage/js/registerBikeParts.js
+++ Project1/Interactive Bike Garage/js/registerBikeParts.js
@@ -1,19 +1,19 @@
 
-let toolbox = document.querySelector(".toolbox");
-
+let currentBikeId = null;
+let bikeInfo = null;
 
 document.addEventListener("click", function (event) {
 
     if (event.target.closest(".bikeObject")) {
 
-        console.log(event.target.closest(".bikeObject").id);
-
         let bikeId = event.target.closest(".bikeObject").id
+        currentBikeId = bikeId;
 
         let bikeDataString = localStorage.getItem(bikeId);
         let fetchedData = JSON.parse(bikeDataString);
-        console.log(fetchedData);
+        bikeInfo = fetchedData;
 
+        // here we apply everything we already know to the fields we want. Preexisting data is bikeName, bikeColor and bikeType
         let infographicTitle = document.getElementById("infographic-name")
         infographicTitle.style.color = fetchedData.bikeColor;
         infographicTitle.innerHTML = "Anatomy Of " + fetchedData.bikeName;
@@ -24,11 +24,13 @@ document.addEventListener("click", function (event) {
         toolboxBikeName.innerHTML = fetchedData.bikeName;
         toolboxBikeName.style.color = fetchedData.bikeColor;
 
+        logKnowndata();
 
         //displays and move the view to the parts registry zone
         let partsZone = document.querySelector(".partsZone");
         partsZone.style.display = "grid";
         partsZone.scrollIntoView({ behavior: "smooth" });
+
 
     }
 
@@ -53,3 +55,83 @@ document.addEventListener("click", function (event) {
 //     };
 // };
 
+
+let submitRegister = document.getElementById("complete-bike-details");
+
+submitRegister.addEventListener("submit", function (event) {
+    event.preventDefault();
+    registerParts();
+    logKnowndata();
+})
+
+
+function registerParts() {
+
+    let bikeData = {
+        bikeName: document.getElementById("toolboxNameInput").value,
+        bikeColor: bikeInfo.bikeColor,
+        bikeType: document.getElementById("toolboxTypeInput").value,
+        bikeHandlebars: document.getElementById("handlebarsInput").value,
+        bikeFrame: document.getElementById("frameInput").value,
+        bikeSize: document.getElementById("sizeInput").value,
+        bikeWheels: document.getElementById("wheelsInput").value,
+        bikeTires: document.getElementById("tiresInput").value,
+        bikeRearDerailleur: document.getElementById("rearDerailleurInput").value,
+        bikeFrontDerailleur: document.getElementById("frontDerailleurInput").value,
+        bikeCassette: document.getElementById("cassetteInput").value,
+        bikePlateau: document.getElementById("plateauInput").value,
+        bikeCranks: document.getElementById("cranksInput").value,
+        bikePedals: document.getElementById("pedalsInput").value,
+    }
+
+    let key = currentBikeId;
+
+    localStorage.setItem(key, JSON.stringify(bikeData));
+
+    let bikeDataString = localStorage.getItem(currentBikeId);
+    bikeInfo = JSON.parse(bikeDataString);
+    console.log(bikeInfo);
+
+
+}
+
+function logKnowndata() {
+
+    document.getElementById("toolboxNameInput").value = bikeInfo.bikeName;
+    document.getElementById("toolboxTypeInput").value = bikeInfo.bikeType;
+
+    //only log info if we already have the tags created in local storage. else, let user define info for the first time
+    if (bikeInfo.bikeHandlebars) {
+        document.getElementById("handlebarsInput").value = bikeInfo.bikeHandlebars;
+        document.getElementById("frameInput").value = bikeInfo.bikeFrame;
+        document.getElementById("sizeInput").value = bikeInfo.bikeSize;
+        document.getElementById("wheelsInput").value = bikeInfo.bikeWheels;
+        document.getElementById("tiresInput").value = bikeInfo.bikeTires;
+        document.getElementById("rearDerailleurInput").value = bikeInfo.bikeRearDerailleur;
+        document.getElementById("frontDerailleurInput").value = bikeInfo.bikeFrontDerailleur;
+        document.getElementById("cassetteInput").value = bikeInfo.bikeCassette;
+        document.getElementById("plateauInput").value = bikeInfo.bikePlateau;
+        document.getElementById("cranksInput").value = bikeInfo.bikeCranks;
+        document.getElementById("pedalsInput").value = bikeInfo.bikePedals;
+    }
+    else {
+        document.getElementById("handlebarsInput").value = "";
+        document.getElementById("frameInput").value = "";
+        document.getElementById("sizeInput").value = "";
+        document.getElementById("wheelsInput").value = "";
+        document.getElementById("tiresInput").value = "";
+        document.getElementById("rearDerailleurInput").value = "";
+        document.getElementById("frontDerailleurInput").value = "";
+        document.getElementById("cassetteInput").value = "";
+        document.getElementById("plateauInput").value = "";
+        document.getElementById("cranksInput").value = "";
+        document.getElementById("pedalsInput").value = "";
+
+    }
+}
+
+let eraseButton = document.querySelector(".erase-memory-button");
+eraseButton.addEventListener("click", function () {
+    localStorage.clear();
+    window.location.reload();
+})
