6b7fac6e9a2796cd61c5440db5f05406c450f0dc
EmileBedard <emile.bed@outlook.fr>
2026-03-17 14:20:39 -0400

    successfull cats generator picker using fetch and a public API

--- learning material/JSONreview/js/jsonFetch.js
+++ learning material/JSONreview/js/jsonFetch.js
@@ -1,6 +1,13 @@
-window.onload = goApp;
 
-async function goApp(){
-console.log("hello fetch")
+window.onload = goFetch;
 
+async function goFetch() {
+    try {
+        let response = await fetch('../files/tests.json');
+        let data = await response.json();
+        console.log(data);
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
