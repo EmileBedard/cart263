d7e344e420f0a29c269054b8c48a98f6330112d2
EmileBedard <emile.bed@outlook.fr>
2026-03-24 16:49:29 -0400

    finished the learning materials

--- learning material/Three.js-I/main.js
+++ learning material/Three.js-I/main.js
@@ -3,12 +3,72 @@ import * as THREE from 'three';
 
 //SCENE
 const scene = new THREE.Scene()
+const loader = new THREE.TextureLoader();
+const group = new THREE.Group();
+scene.add(group);
 
 //A: the geometry
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+
 //B: the material
-const material = new THREE.MeshBasicMaterial({ color: 0x800080 })
-//C: put together
+
+const water_texture = await loader.loadAsync('textures/Ice002_1K-JPG_Color.jpg');
+//need to ensure that the textures are encoded correctly - mapping the colors correctly.
+water_texture.colorSpace = THREE.SRGBColorSpace;
+
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
-//D: ADD TO THE SCENE
 scene.add(mesh)
+
+const mesh_2 = new THREE.Mesh(geometry, material)
+scene.add(mesh_2)
+mesh_2.position.x = -2
+
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+scene.add(camera)
+
+//TURN ON AXES HELPER
+//https://threejs.org/docs/?q=Axes#AxesHelper
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
+//move it 
+axesHelper.position.x = -1;
+axesHelper.position.y = -1;
+
+//move camera
+camera.position.z = 2;
+
+//Access the Canvas
+const canvas = document.querySelector('canvas#three-ex')
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+//give it the size
+renderer.setSize(sizes.width, sizes.height)
+
+//render:
+window.requestAnimationFrame(animate)
+let elapsedTime = 0  //
+function animate(timer) {
+    //calculate the difference since last frame
+    let deltaTime = timer - elapsedTime
+    elapsedTime = timer //update  new elapsedTime
+    //console.log(deltaTime)
+    mesh.rotation.x += 0.01 * deltaTime
+
+    // Update objects -> elapsed time increases ...
+    mesh_2.position.x = Math.cos(elapsedTime / 1000)
+    mesh_2.position.y = Math.sin(elapsedTime / 1000)
+
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(animate)
+
+}
