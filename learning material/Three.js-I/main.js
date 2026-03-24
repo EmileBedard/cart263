36f5139e0df476782d49947b6e2c14bf853730c6
EmileBedard <emile.bed@outlook.fr>
2026-03-24 14:41:40 -0400

    added the first mesh to my scene

--- learning material/Three.js-I/main.js
+++ learning material/Three.js-I/main.js
@@ -1,2 +1,14 @@
 // library ref: because we are loading a module
 import * as THREE from 'three';
+
+//SCENE
+const scene = new THREE.Scene()
+
+//A: the geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+//B: the material
+const material = new THREE.MeshBasicMaterial({ color: 0x800080 })
+//C: put together
+const mesh = new THREE.Mesh(geometry, material)
+//D: ADD TO THE SCENE
+scene.add(mesh)
