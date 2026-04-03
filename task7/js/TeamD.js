21e77ac814be1ef1a466b391463f6639fa9e7b1d
EmileBedard <emile.bed@outlook.fr>
2026-04-03 12:58:13 -0400

    added first gltf, need to add the other 2

--- task7/js/TeamD.js
+++ task7/js/TeamD.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 
 // Planet class for Team D
 export class PlanetD {
@@ -8,8 +10,16 @@ export class PlanetD {
         this.orbitSpeed = orbitSpeed;
         this.angle = Math.random() * Math.PI * 2;
 
+        this.summerPlanet = null;
+        this.autumnPlanet = null;
+        this.winterPlanet = null;
+
         //Create planet group
         this.group = new THREE.Group()
+
+        //load and run planets gltf!!
+        this.loadAndRunModels(this.group);
+
 
         // Create planet
         //STEP 1:
@@ -17,6 +27,7 @@ export class PlanetD {
         //TODO: Give it a custom material using THREE.MeshStandardMaterial.
         //TODO: Use castShadow and receiveShadow on the mesh and all future ones so they can cast and receive shadows.
         //TODO: Add the planet mesh to the planet group.
+
 
         const planetGeo = new THREE.SphereGeometry(2, 70, 32); // radius between 1.5 and 2
         const planetMat = new THREE.MeshStandardMaterial({
@@ -78,9 +89,65 @@ export class PlanetD {
         this.group.rotation.y += delta * 0.5;
 
         //TODO: Do the moon orbits and the model animations here.
+        if (this.summerPlanet) {
+
+        }
     }
 
     click(mouse, scene, camera) {
         //TODO: Do the raycasting here.
     }
+
+    async loadAndRunModels(objsGroup) {
+
+        // gltf loader
+        const gltfLoader = new GLTFLoader();
+
+        // add gltf model
+        let gltfModel1 = null;
+        let gltfModel2 = null;
+        let gltfModel3 = null;
+
+        try {
+            gltfModel1 = await gltfLoader.loadAsync('models/summer-planet-task7-cart263.gltf');
+            //gltfModel2 = await gltfLoader.loadAsync('models/summer-planet-task7-cart263.gltf');
+            //gltfModel3 = await gltfLoader.loadAsync('models/summer-planet-task7-cart263.gltf');
+
+            let objs = []
+            objs.push(gltfModel1)
+            //objs.push(gltfModel2)
+            //objs.push(gltfModel3)
+        }
+        catch (error) {
+            console.log(error.message)
+        }
+
+        //setup planet1 -- summer
+        let summerPlanet = gltfModel1.scene;
+        let planetLight = summerPlanet.getObjectByName('planet_light');
+        let lanternLight = summerPlanet.getObjectByName('lantern');
+
+        //setup lights
+        planetLight.intensity = 0;
+
+        lanternLight.intensity = 2;
+        lanternLight.color.set(0xFF8B2C);
+
+        //transform
+        summerPlanet.position.set(0, 0, 0);
+        summerPlanet.scale.set(3, 3, 3);
+
+        //setup shadows
+        summerPlanet.castShadow = true;
+        summerPlanet.receiveShadow = true;
+
+
+        this.summerPlanet = summerPlanet;
+        //this.autumnPlanet = autumnPlanet;
+        //this.winterPlanet = winterPlanet;
+        //push to group
+        objsGroup.add(summerPlanet);
+        console.log(summerPlanet);
+    }
 }
+
