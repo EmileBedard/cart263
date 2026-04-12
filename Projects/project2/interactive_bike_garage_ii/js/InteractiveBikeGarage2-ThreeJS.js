3cd8392123f60131bee343a10830ed15e0fb8b38
EmileBedard <emile.bed@outlook.fr>
2026-04-12 13:08:18 -0400

    added the new textures and glb files for the three objects

--- Projects/project2/interactive_bike_garage_ii/js/InteractiveBikeGarage2-ThreeJS.js
+++ Projects/project2/interactive_bike_garage_ii/js/InteractiveBikeGarage2-ThreeJS.js
@@ -66,25 +66,24 @@ let garageModel = null;
 loadAndRunModels();
 async function loadAndRunModels(objectsGroup) {
     try {
-        const loader = new HDRLoader();
 
-        // // 2. Load your HDR file (make sure to update the path to your actual file)
+        //Load HDR file
+        const loader = new HDRLoader();
         const hdrTexture = await loader.loadAsync('assets/drakensberg_solitary_mountain_1k.hdr');
         console.log(hdrTexture);
-
-        // // 3. Set the mapping so Three.js knows it's a 360/spherical image
+        //map a 360 image
         hdrTexture.mapping = THREE.EquirectangularReflectionMapping;
-
-        // // 4. Apply it to the scene! 
-        //scene.environment = hdrTexture;
-        // console.log(scene.environment)
-
-        // // Optional: Uncomment the next line if you also want to see the HDR as the background skybox
         scene.background = hdrTexture;
 
+        //load blender scene from gltf
         garageModel = await gltfLoader.loadAsync('assets/garage-v0/project2-interactiveBikeGarage-cart263.gltf');
-        let objs = [] //keeping the array if i need more gltf later
-        objs.push(garageModel);
+        //load cabinet gltf
+        cabinetModel = await gltfLoader.loadAsync('assets/garage-v0/project2-interactiveBikeGarage-cart263.gltf');
+        //load workbench gltf
+        workbenchModel = await gltfLoader.loadAsync('assets/garage-v0/project2-interactiveBikeGarage-cart263.gltf');
+
+        let objs = []
+        objs.push(garageModel, cabinetModel, workbenchModel);
 
         addAndRun(objs)
 
